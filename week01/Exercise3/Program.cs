dc00d6ff6d717a704ae0d86cf3b3eb4a9bc33edb
adele <adeley1106@gmail.com>
2025-07-03 03:21:27 -0400

    Loops assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        // Console.WriteLine("Enter the magic number");
+        // string magic_number = Console.ReadLine();
+        // int magic_num_con = int.Parse(magic_number);
+
+        int userGuess = 0;
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine($"Guess the magic number");
+            string guess = Console.ReadLine();
+            userGuess = int.Parse(guess);
+
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine($"Your guess is higher, try next!");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine($"Your guess is lower, try next!");
+            }
+            else
+            {
+                Console.WriteLine($"Wow!You guessed right!");
+            }
+        }
     }
 }
