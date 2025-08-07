212dbd8177320da78e00032e99a7146791c605cf
adele <adeley1106@gmail.com>
2025-08-07 11:52:30 +0100

    Inheritance exercise

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("John Doe","Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        //Mathassignment class
+        MathAssignment assignment2 = new MathAssignment("Iyanu Adeleye","Addition","7.3","8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        //Writingassignment class
+        WritingAssignment assignment3 = new WritingAssignment("Martins Lilian","Fractions","Fundamental Algebra");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWrittingInformation());
     }
 }
